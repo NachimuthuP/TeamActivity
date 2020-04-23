f42533a063bd60e7fde3c941d9021f9e16c0de2d
NachimuthuP <nachirahul@gmail.com>
2020-04-23 19:48:52 +1000

    sencond

--- app/src/main/java/com/nachi/teamactivity/MainActivity.kt
+++ app/src/main/java/com/nachi/teamactivity/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.nachi.teamactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : LogActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val username = findViewById<EditText>(R.id.empid)
+        username.isPressed()
+
+
+        siginin.setOnClickListener{
+            val username1 = username.text.toString()
+
+            if(username1=="593083") {
+                val startpage = Intent(this, firstscreen1::class.java)
+                startActivity(startpage)
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/nachi/teamactivity/firstscreen1.kt
@@ -0,0 +1,12 @@
+package com.nachi.teamactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class firstscreen1 : LogActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firstscreen)
+    }
+}
