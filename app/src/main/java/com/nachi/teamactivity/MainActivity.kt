917e84993db07fe61330b73801b52af8fdd30332
NachimuthuP <nachirahul@gmail.com>
2020-04-24 21:06:48 +1000

    Till Navigation drawer

--- app/src/main/java/com/nachi/teamactivity/MainActivity.kt
+++ app/src/main/java/com/nachi/teamactivity/MainActivity.kt
@@ -3,8 +3,11 @@ package com.nachi.teamactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 public class MainActivity : LogActivity() {
@@ -15,17 +18,18 @@ public class MainActivity : LogActivity() {
         setContentView(R.layout.activity_main)
         val username = findViewById<EditText>(R.id.empid)
         username.isPressed()
+    
 
+            siginin.setOnClickListener {
+                val username1 = username.text.toString()
 
-        siginin.setOnClickListener{
-            val username1 = username.text.toString()
-
-            if(username1=="593083") {
-                val startpage = Intent(this, firstscreen1::class.java)
-                startActivity(startpage)
+                if (username1 == "593083") {
+                    val startpage = Intent(this, firstscreen1::class.java)
+                    startActivity(startpage)
+                }
             }
+
+
         }
-
-
     }
-}
+
--- app/src/main/java/com/nachi/teamactivity/firstscreen1.kt
+++ app/src/main/java/com/nachi/teamactivity/firstscreen1.kt
@@ -2,11 +2,84 @@ package com.nachi.teamactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.firstscreen.*
 
-class firstscreen1 : LogActivity() {
-
+class firstscreen1 : LogActivity(), NavigationView.OnNavigationItemSelectedListener {
+    lateinit var navView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.firstscreen)
+        setContentView(R.layout.nav_drawer)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        val drawerToggle:ActionBarDrawerToggle=object : ActionBarDrawerToggle(
+            this,drawerLayout,toolbar,(R.string.open),(R.string.close)
+        ){
+        }
+        drawerToggle.isDrawerIndicatorEnabled=true
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.action_settings -> {
+                    Toast.makeText(applicationContext, "click on setting", Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.action_share -> {
+                    Toast.makeText(applicationContext, "click on share", Toast.LENGTH_LONG).show()
+                    return true
+                }
+                R.id.action_exit -> {
+                    Toast.makeText(applicationContext, "click on exit", Toast.LENGTH_LONG).show()
+                    return true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+}
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_message -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_chat -> {
+                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_share -> {
+                Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_send -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+    }else{
+        super.onBackPressed()
     }
 }
+}
+
+
+
